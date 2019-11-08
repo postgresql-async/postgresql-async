a2cd2e7ad938f0609c4ff003dcd452c6b9733cb0
jilen <jilen.zhang@gmail.com>
2019-11-08 19:59:51 +0800

    fix sql

--- script/prepare_mysql.sql
+++ script/prepare_mysql.sql
@@ -6,4 +6,4 @@ CREATE USER 'mysql_async_nopw'@'localhost' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON *.* TO 'mysql_async'@'localhost';
 GRANT ALL PRIVILEGES ON *.* TO 'mysql_async_old'@'localhost';
 UPDATE mysql.user SET Password = OLD_PASSWORD('do_not_use_this'), plugin = 'mysql_old_password' where User = 'mysql_async_old'; flush privileges;
-GRANT ALL PRIVILEGES ON *.* TO 'mysql_async_nopw'@'localhost' WITH GRANT OPTION
+GRANT ALL PRIVILEGES ON *.* TO 'mysql_async_nopw'@'localhost';
