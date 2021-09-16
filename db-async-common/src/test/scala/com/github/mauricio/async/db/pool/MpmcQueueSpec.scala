46b14b46ae68b71055a74da7792a90dfc4bb3fb3
jilen <jilen.zhang@gmail.com>
2021-09-16 11:34:42 +0800

    fix

--- db-async-common/src/test/scala/com/github/mauricio/async/db/pool/MpmcQueueSpec.scala
+++ db-async-common/src/test/scala/com/github/mauricio/async/db/pool/MpmcQueueSpec.scala
@@ -62,6 +62,7 @@ class MpmcQueueSpec extends Specification with ScalaCheck {
 
   private def dequeueWithLatch[A](
     queue: MpmcQueue[A],
+    dequeueFunc: MpmcQueue[A] => Option[A],
     consumeSemaphore: Semaphore,
     latch: CountDownLatch
   ) = {
@@ -76,51 +77,90 @@ class MpmcQueueSpec extends Specification with ScalaCheck {
     }(ExecutionContext.global)
   }
 
-  val parallelEnqueueDequeue = {
-    Prop.forAll { data: Data =>
-      val queue             = MpmcQueue[Int](data.capacity)
-      val elems             = data.input
-      val latch             = new CountDownLatch(1)
-      val consumeSemaphore  = new Semaphore(data.capacity)
-      val producerSemaphore = new Semaphore(data.capacity)
-
-      val f1 = Future.sequence(
-        Vector.fill(elems.size * 2)(
-          dequeueWithLatch(queue, consumeSemaphore, latch)
-        )
-      )
-      val f2 = Future.traverse(elems) { e =>
-        enqueueWithLatch(queue, e, producerSemaphore, latch)
-      }
-      latch.countDown()
-      val r1 = Await.result(f1, duration.Duration.Inf)
-      val r2 = Await.result(f2, duration.Duration.Inf)
-      val remain = Iterator
-        .continually(queue.take())
-        .takeWhile(_.isDefined)
-        .toVector
-        .collect { case Some(e) =>
-          e
-        }
-      val enqueueR    = r2.toMap
-      val dequeueR    = r1.collect { case Some(e) => e }
+  private case class RunResult(
+    enqueueR: Vector[(Int, Boolean)],
+    dequeueR: Vector[Int],
+    remain: Vector[Int]
+  ) {
+    def verify() = {
       val remainSize  = remain.size
-      val enqueueSize = r2.filter(_._2).size
+      val enqueueSize = enqueueR.filter(_._2).size
       val dequeueSize = dequeueR.size
 
       val itemsMatch = (dequeueR ++ remain).forall { e =>
-        val found = r2.exists { case (e1, success) =>
+        enqueueR.exists { case (e1, success) =>
           (e1 == e && success == true)
         }
-        if (!found) {
-          println(s"${e} not enqueued ${r2.find(_._1 == e)}")
-        }
-        found
       }
       itemsMatch && (dequeueSize + remainSize) == enqueueSize
     }
   }
 
+  private def runParallel(
+    data: Data,
+    dequeueFunc: MpmcQueue[Int] => Option[Int]
+  ): RunResult = {
+    val queue             = MpmcQueue[Int](data.capacity)
+    val elems             = data.input
+    val latch             = new CountDownLatch(1)
+    val consumeSemaphore  = new Semaphore(data.capacity)
+    val producerSemaphore = new Semaphore(data.capacity)
+
+    val f1 = Future.sequence(
+      Vector.fill(elems.size * 2)(
+        dequeueWithLatch(queue, dequeueFunc, consumeSemaphore, latch)
+      )
+    )
+    val f2 = Future.traverse(elems) { e =>
+      enqueueWithLatch(queue, e, producerSemaphore, latch)
+    }
+    latch.countDown()
+    val r1       = Await.result(f1, duration.Duration.Inf)
+    val r2       = Await.result(f2, duration.Duration.Inf)
+    val dequeueR = r1.collect { case Some(e) => e }
+    val remain = Iterator
+      .continually(queue.take())
+      .takeWhile(_.isDefined)
+      .toVector
+      .collect { case Some(e) =>
+        e
+      }
+    RunResult(r2, dequeueR, remain)
+  }
+
+  val parallelEnqueueDequeue = {
+    Prop.forAll { data: Data =>
+      val rr = runParallel(data, _.take())
+      rr.verify()
+    }
+  }
+
+  val runParallelWithPeek = {
+    Prop.forAll { data: Data =>
+      val rr = runParallel(
+        data,
+        q => {
+          q.peek()
+          q.take()
+        }
+      )
+      rr.verify()
+    }
+  }
+
+  val peekLastElem = {
+    Prop.forAll { data: Data =>
+      val queue = MpmcQueue[Int](data.capacity)
+      data.input.map { i =>
+        (queue.offer(i), queue.peek() == Some(i), queue.take() == Some(i))
+      }.forall { case (r1, r2, r3) =>
+        r1 && r2 && r3
+      }
+    }
+  }
+
   s2"queue must enqueue / dequeue in fifo order ${fifoNoOverflow}"
   s2"enqueue/deuque correctly during parallel access ${parallelEnqueueDequeue}"
+  s2"peek should always return last enqueued element ${peekLastElem}"
+  s2"peek should not affect enqueue/dequeue during parallel access ${runParallelWithPeek}"
 }
