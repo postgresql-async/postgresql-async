569ab76226e9b2f564262faf93f2dccf6c611608
jilen <jilen.zhang@gmail.com>
2021-09-17 16:00:51 +0800

    save

--- db-async-common/src/main/scala/com/github/mauricio/async/db/pool/SelfHealing.scala
+++ db-async-common/src/main/scala/com/github/mauricio/async/db/pool/SelfHealing.scala
@@ -1,15 +1,33 @@
 package com.github.mauricio.async.db.pool
 
+import java.util.concurrent.atomic._
 import scala.concurrent.{Future, Promise}
+import org.slf4j.LoggerFactory
 
 /**
  * A self-healing lazy, async inited resource (connection, for example)
  */
 private[db] trait SelfHealing[A] {
+
+  /**
+   * Get the resource instance, recreate if dead
+   */
   def get(): Future[A]
+
+  /**
+   * Release the resource now. If [[get]] is called later, resource will be
+   * recreate
+   */
+  def releaseNow(): Future[A]
 }
 
 object SelfHealing {
+
+  case class Config(
+    checkInterval: Long,
+    checkTimeout: Long,
+    minHealInterval: Long
+  )
 
   /**
    * Create swappable resource
@@ -17,14 +35,46 @@ object SelfHealing {
   def apply[A](
     acquire: () => Future[A],
     release: A => Future[Unit],
-    check: A => Future[Boolean]
+    check: A => Future[Boolean],
+    config: Config
   ): SelfHealing[A] = {
     ???
   }
 
   private sealed trait State[+A]
-  private case object Idle                                extends State[Nothing]
-  private case class Initializing[A](promise: Promise[A]) extends State[A]
-  private case class Ready[A](item: A)                    extends State[A]
-  private case class Healing[A](dead: A, replacer: Promise[A]) extends State[A]
+
+  private case object Idle extends State[Nothing]
+
+  private case class Inited[A](createTime: Long, promise: Promise[A])
+      extends State[A]
+
+  private class SelfHealingImpl[A](
+    acquire: () => Future[A],
+    release: A => Future[Unit],
+    check: A => Future[Boolean],
+    config: Config
+  ) extends SelfHealing[A] {
+
+    val state = new AtomicReference[State[A]](Idle)
+
+    def get(): Future[A] = {
+      state.get() match {
+        case Idle =>
+          val p = Promise[A]()
+          if (
+            state.compareAndSet(Idle, Inited(System.currentTimeMillis(), p))
+          ) {
+            p.completeWith(acquire())
+            p.future
+          } else get()
+        case i @ Inited(t, p) =>
+          p.future
+      }
+    }
+
+    def releaseNow(): Future[A] = {
+      ???
+    }
+  }
+
 }
