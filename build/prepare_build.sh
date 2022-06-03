d2d814ccbb3f8979725e3462ba3cea8a42a77195
Alexandre Balon-Perin <alexbp13@gmail.com>
2022-06-03 23:28:00 +0900

    other revert

--- build/prepare_build.sh
+++ build/prepare_build.sh
@@ -10,8 +10,8 @@ until mysqladmin ping -h'127.0.0.1' --silent || [ $RETRIES -eq 0 ]; do
     sleep 3
 done
 
-PGRETRIES=5
-until psql -h $PG_HOST -U postgres -d template1 -c "select 1" > /dev/null 2>&1 || [ $PGRETRIES -eq 0 ]; do
+RETRIES=5
+until psql -h $PG_HOST -U postgres -d template1 -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
   echo "Waiting for postgres server, $((RETRIES--))..."
   sleep 3
 done
