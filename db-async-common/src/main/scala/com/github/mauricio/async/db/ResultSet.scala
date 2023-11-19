f3d82b9cc8067e5009d6a879e646bc5cbdd9ee62
Scala Steward <scala-steward@users.noreply.github.com>
2023-11-19 01:19:53 +0000

    Reformat with scalafmt 3.7.17
    Executed command: scalafmt --non-interactive

--- db-async-common/src/main/scala/com/github/mauricio/async/db/ResultSet.scala
+++ db-async-common/src/main/scala/com/github/mauricio/async/db/ResultSet.scala
@@ -21,7 +21,7 @@ import scala.collection.immutable.IndexedSeq
 
 /**
  * Represents the collection of rows that is returned from a statement inside a
- * {@link QueryResult}. It's basically a collection of Array[Any]. Mutating
+ * {@link QueryResult} . It's basically a collection of Array[Any]. Mutating
  * fields in this array will not affect the database in any way
  */
 trait ResultSet extends IndexedSeq[RowData] {
